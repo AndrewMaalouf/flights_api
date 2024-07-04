c55e5fac0efe9fde9e4a5c84b9d32061400f5c94
Andrew Maalouf <andrewmaalouf11@gmail.com>
2024-07-04 15:55:38 +0300

    Creating Flight model and seeding database

--- database/migrations/2024_07_04_100143_create_passengers_table.php
+++ database/migrations/2024_07_04_100143_create_passengers_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('first_name');
             $table->string('last_name');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->string('password');
             $table->date('dob');
             $table->date('passport_expiry_date');
